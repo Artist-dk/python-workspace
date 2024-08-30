90698d6df036ede6f9ea90ce758a09a7d15dde72
CyberSpy-kernel <cyberspy.kernel@gmail.com>
2024-08-30 23:38:01 +0530

    created new wordlist genrator. this wordlist genrates list of email id

---
+++ hck/wordlist/main.py
@@ -0,0 +1,43 @@
+import sys
+import time
+from wordlist_generator import generate_wordlist
+from email_generator import generate_email_list, domains
+from .utils import ProgressMsg, clear_memory
+
+min_length = 6
+max_length = 7
+characters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+wordlist_directory = 'K:/wordlist/temp'
+
+def main():
+    generate_emails = False
+    if len(sys.argv) > 1 and sys.argv[1] == "--emails":
+        generate_emails = True
+
+    progress = ProgressMsg()
+    if generate_emails:
+        total_combinations = sum(len(characters) ** length for length in range(min_length, max_length + 1))
+        progress.total_combinations = total_combinations
+        progress.calculate_required_storage_size(min_length, max_length, total_combinations)
+
+        start_time = time.time()
+        generate_email_list(characters, min_length, max_length, wordlist_directory, domains, progress)
+    else:
+        total_combinations = sum(len(characters) ** length for length in range(min_length, max_length + 1))
+        progress.total_combinations = total_combinations
+        progress.calculate_required_storage_size(min_length, max_length, total_combinations)
+
+        start_time = time.time()
+        generate_wordlist(characters, min_length, max_length, wordlist_directory, progress)
+
+    progress.elapsed_time = time.time() - start_time
+    progress.update_remaining_time_chunks(
+        (progress.elapsed_time / (progress.wordcount / progress.total_combinations)) - progress.elapsed_time
+    )
+    progress.generate_progress_msg()
+    sys.stdout.write('\033[F\033[K' * 25)
+    sys.stdout.write(progress.progress_msg)
+    sys.stdout.flush()
+
+if __name__ == "__main__":
+  main()
---
+++ hck/wordlist/wordlist_generator.py
@@ -0,0 +1,50 @@
+import itertools
+import os
+from utils import clear_memory
+
+def generate_wordlist(characters, min_length, max_length, wordlist_directory, progress):
+    wordcount = 0
+    batch_size = 10000000
+    current_batch = []
+    batch_index = 1
+    used_storage = 0
+    filename = ''
+
+    for length in range(min_length, max_length + 1):
+        for combination in itertools.product(characters, repeat=length):
+            word = ''.join(combination).strip()
+            wordcount += 1
+            current_batch.append(word)
+
+            if len(current_batch) == batch_size:
+                filename = save_wordlist_to_file(current_batch, wordlist_directory, 'wordlist', batch_index)
+                used_storage += os.path.getsize(filename)
+                clear_memory()
+                current_batch = []
+                batch_index += 1
+
+            if wordcount % 100000 == 0:
+                update_progress(progress, wordcount, used_storage, batch_index, filename)
+
+    if current_batch:
+        filename = save_wordlist_to_file(current_batch, wordlist_directory, 'wordlist', batch_index)
+        used_storage += os.path.getsize(filename)
+        clear_memory()
+        batch_index += 1
+
+def save_wordlist_to_file(wordlist, wordlist_directory, base_filename, batch_index):
+    os.makedirs(wordlist_directory, exist_ok=True)
+    filename = f'{wordlist_directory}/{base_filename}_{batch_index}.txt'
+    with open(filename, 'w') as file:
+        file.write('\n'.join(wordlist))
+    return filename
+
+def update_progress(progress, wordcount, used_storage, batch_index, filename):
+    progress.batch_index = batch_index
+    progress.wordcount = wordcount
+    progress.calculate_used_storage(used_storage)
+    progress.filename = filename
+    progress.generate_progress_msg()
+    sys.stdout.write('\033[F\033[K' * 19)
+    sys.stdout.write(progress.progress_msg)
+    sys.stdout.flush()
