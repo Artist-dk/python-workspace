b4d761d5cb2e613a5d97d79a6e6887f16a3e35c7
CyberSpy-kernel <cyberspy.kernel@gmail.com>
2024-08-31 19:39:55 +0530

    added variables for email generation

--- hck/Final-list-creator/config.py
+++ hck/Final-list-creator/config.py
@@ -1,7 +1,13 @@
 # Configuration settings
 
+# For password generation
 min_length = 6
 max_length = 7
 characters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # characters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=`~\""
-list_directory = '/home/kernel/Desktop/Wordlist/temp1'
+list_directory = '/home/kernel/Desktop/Wordlist/temp'
+
+# For email generation
+name_file = 'name.txt'
+email_domains = ['gmail.com', 'yahoo.com', 'hotmail.com']
+email_directory = '/home/kernel/Desktop/Emails'
